3a95360bd3e1a43fb759e6d5d4d3ad61df12f92a
Okan Erdem <nayko62@gmail.com>
2023-01-29 23:33:18 +0300

    animations but its kinda buggy atm

--- components/home/InfoSection.tsx
+++ components/home/InfoSection.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 import People from '@/data/Data';
 import NameSection from '@/components/home/NameSection';
 import InfoTable from '@/components/home/InfoTable';
@@ -6,6 +7,9 @@ import styles from '@/styles/Home.module.css';
 
 const InfoSection = () => {
   const [selectedName, setSelectedName] = useState<string>('john');
+  const [parent] = useAutoAnimate<HTMLDivElement>({
+    duration: 450,
+  });
 
   const nameHandler = (e: any) => {
     const name = e.target.innerText.toLowerCase();
@@ -16,10 +20,12 @@ const InfoSection = () => {
     <>
       <section className={styles.sections}>
         <NameSection selectedName={selectedName} nameHandler={nameHandler} />
-        {People.map((person, index) => {
-          if (person.name.toLowerCase() !== selectedName) return null;
-          return <InfoTable key={index} person={person} />;
-        })}
+        <div ref={parent}>
+          {People.map((person, index) => {
+            if (person.name.toLowerCase() !== selectedName) return null;
+            return <InfoTable key={index} person={person} />;
+          })}
+        </div>
       </section>
     </>
   );
