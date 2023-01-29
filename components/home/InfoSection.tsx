b4a67d336f0b509d765c058346e69bcbd5a698c0
Okan Erdem <nayko62@gmail.com>
2023-01-29 21:01:41 +0300

    cleaner index

--- components/home/InfoSection.tsx
+++ components/home/InfoSection.tsx
@@ -1,30 +1,48 @@
+import { useState } from 'react';
 import People from '@/data/Data';
 import styles from '@/styles/Home.module.css';
+import NameSection from './NameSection';
 
-interface InfoSectionProps {
-  selectedName: string;
-}
+const InfoSection = () => {
+  const [selectedName, setSelectedName] = useState<string>('john');
 
-const InfoSection = ({ selectedName }: InfoSectionProps) => {
   const person = People.find(
     person => person.name.toLowerCase() === selectedName
   );
 
+  const nameHandler = (e: any) => {
+    const name = e.target.innerText.toLowerCase();
+    setSelectedName(name);
+  };
+
   return (
-    <div className={styles.infoSection}>
-      <div className={styles.info}>
-        <p className="text-gray-500 dark:text-gray-400">{person?.job}</p>
-        <p className="text-gray-500 dark:text-gray-400">{person?.duration}</p>
-      </div>
-      <div className={styles.info}>
-        <section className="flex flex-col gap-2">
-          <p className="text-gray-500 dark:text-gray-400">{person?.info}</p>
-          <p className="text-gray-500 dark:text-gray-400">{person?.info2}</p>
-          <p className="text-gray-500 dark:text-gray-400">{person?.info3}</p>
-          <p className="text-gray-500 dark:text-gray-400">{person?.info4}</p>
-        </section>
-      </div>
-    </div>
+    <>
+      <section className={styles.sections}>
+        <NameSection selectedName={selectedName} nameHandler={nameHandler} />
+        <div className={styles.infoSection}>
+          <div className={styles.info}>
+            <p className="text-gray-500 dark:text-gray-400">{person?.job}</p>
+            <p className="text-gray-500 dark:text-gray-400">
+              {person?.duration}
+            </p>
+          </div>
+          <div className={styles.info}>
+            <section className="flex flex-col gap-2">
+              <p className="text-gray-500 dark:text-gray-400">{person?.info}</p>
+              <p className="text-gray-500 dark:text-gray-400">
+                {person?.info2}
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                {person?.info3}
+              </p>
+              <p className="text-gray-500 dark:text-gray-400">
+                {person?.info4}
+              </p>
+            </section>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
 import Header from '@/components/Header';
 import Title from '@/components/home/Title';
-import NameSection from '@/components/home/NameSection';
 import InfoSection from '@/components/home/InfoSection';
 
 const Home = () => {
@@ -10,21 +8,11 @@ const Home = () => {
   const content =
     'Tabs App with Next.js & Tailwind CSS & TypeScript & AutoAnimate';
 
-  const [selectedName, setSelectedName] = useState<string>('john');
-
-  const nameHandler = (e: any) => {
-    const name = e.target.innerText.toLowerCase();
-    setSelectedName(name);
-  };
-
   return (
     <>
       <Header title={header} content={content} />
       <Title title={title} />
-      <div className="flex flex-col md:flex-row  max-w-2xl w-full items-center mx-auto gap-10">
-        <NameSection selectedName={selectedName} nameHandler={nameHandler} />
-        <InfoSection selectedName={selectedName} />
-      </div>
+      <InfoSection />
     </>
   );
 };
